d3f79f6560941688231e90202d93ba9d442b2d4c
prakharsingh-74 <prakharsingh7014@gmail.com>
2026-02-09 12:05:24 +0530

    feat: Add a new Next.js web application with Convex integration and configure TypeScript for web and backend packages.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,15 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
+'use client'
+
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World/web</h1>
-        <div className="flex gap-2">
-          <Button>Button</Button>
-          <Button variant="outline">Outline</Button>
-          <p>{add(1, 2)}</p>
-        </div>
+      <p>apps/web</p>
+      <div className="max-w-sm w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
       </div>
     </div>
   )
