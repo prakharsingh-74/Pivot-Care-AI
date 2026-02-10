b98ff57ea485c7c9f3502686263dce5c7fc4c78b
prakharsingh-74 <prakharsingh7014@gmail.com>
2026-02-10 14:49:18 +0530

    feat: Clerk authentication and Convex backend integration.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,12 @@
-'use client'
+"use client";
 
-import { useQuery, useMutation } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import {
+  useQuery,
+  useMutation,
+  Authenticated,
+  Unauthenticated,
+} from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 
@@ -8,12 +14,21 @@ export default function Page() {
   const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <p>apps/web</p>
-      <Button onClick={() => addUser()}>Add</Button>
-      <div className="max-w-sm w-full mx-auto">
-        {JSON.stringify(users, null, 2)}
-      </div>
-    </div>
-  )
+    <>
+      <Authenticated>
+        <div className="flex items-center justify-center min-h-svh">
+          <p>apps/web</p>
+          <UserButton />
+          <Button onClick={() => addUser()}>Add</Button>
+          <div className="max-w-sm w-full mx-auto">
+            {JSON.stringify(users, null, 2)}
+          </div>
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <p>Must be Signed in!</p>
+        <SignInButton>Sign in!</SignInButton>
+      </Unauthenticated>
+    </>
+  );
 }
