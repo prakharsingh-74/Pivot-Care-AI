e0b682ea4d8033aab5b900b0e8e3fb25d85e4d5f
prakharsingh-74 <prakharsingh7014@gmail.com>
2026-02-18 09:14:12 +0530

    screen state management using Jotai and Convex.

---
+++ apps/widget/modules/widget/constants.ts
@@ -0,0 +1,12 @@
+export const WIDGET_SCREENS = {
+    ERROR: "error",
+    LOADING: "loading",
+    SELECTION: "selection",
+    VOICE: "voice",
+    AUTH: "auth",
+    INBOX: "inbox",
+    CHAT: "chat",
+    CONTACT: "contact",
+} as const;
+
+export const CONTACT_SESSION_KEY = "pivotcare_contact_session"
--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,19 +1,31 @@
 'use client';
 
-// import { WidgetFooter } from "../components/widget-footer";
-// import { WidgetHeader } from "../components/widget-header";
-import { WidgetAuthScreen } from "../screens/widget-auth-screen";
+import { useAtomValue } from "jotai";
+import { screenAtom } from "@/modules/widget/atoms/widget-atoms";
+import { WidgetAuthScreen } from "@/modules/widget/ui/screens/widget-auth-screen";
 
 interface Props{
     organizationId: string;
 }
 
 export default function WidgetView({organizationId}: Props){
+    const screen = useAtomValue(screenAtom);
+
+    const screenComponents = {
+        error: <p>TODO: Error</p>,
+        loading: <p>TODO: Loading</p>,
+        auth: <WidgetAuthScreen/>,
+        voice: <p>TODO: Voice</p>,
+        inbox: <p>TODO: Inbox</p>,
+        selection: <p>TODO: Selection</p>,
+        chat: <p>TODO: Chat</p>,
+        contact: <p>TODO: Contact</p>,
+    }
+
     return (
         // confirm whether or not min-h-screen is needed
         <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-            <WidgetAuthScreen/>
-            {/* <WidgetFooter/> */}
+            {screenComponents[screen]}
         </main>
     );
 }
