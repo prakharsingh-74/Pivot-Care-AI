6e19ada2f40fc4c98b5cd17f259d07c519caa8b2
prakharsingh-74 <prakharsingh7014@gmail.com>
2026-05-19 22:33:46 +0530

    fix: branding and logo layout

--- apps/web/modules/dashboard/ui/views/conversations-view.tsx
+++ apps/web/modules/dashboard/ui/views/conversations-view.tsx
@@ -4,8 +4,8 @@ export const ConversationsView = () => {
     return (
         <div className="flex h-full flex-1 flex-col gap-y-4 bg-muted">
             <div className="flex flex-1 items-center justify-center gap-x-2">
-                <Image alt="Logo" src="/logo.png" width={40} height={40} />
-                <p className="font-semibold text-lg">Pivot Care AIt</p>
+                <Image alt="Logo" src="/logo.svg" width={40} height={40} />
+                <p className="font-semibold text-lg">Pivot Care AI</p>
             </div>
         </div>
     )
